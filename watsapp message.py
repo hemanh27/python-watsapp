2cc97286502ab536df6f483a9fcb5fdc2544ae7f
hemanh27 <hemanh27@gmail.com>
2017-12-06 21:27:23 +0100

    First commit

---
+++ watsapp message.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
+
+driver = webdriver.Chrome('/Users/apple/downloads/chromedriver')
+
+driver.get("https://web.whatsapp.com/")
+wait = WebDriverWait(driver, 600)
+
+
+target = '"kartheek"'
+
+
+string = "Message sent using Python!!!"
+
+x_arg = '//span[contains(@title,' + target + ')]'
+group_title = wait.until(EC.presence_of_element_located((
+    By.XPATH, x_arg)))
+group_title.click()
+inp_xpath = '//div[@class="input"][@dir="auto"][@data-tab="1"]'
+input_box = wait.until(EC.presence_of_element_located((
+    By.XPATH, inp_xpath)))
+for i in range(100):
+    input_box.send_keys(string + Keys.ENTER)
+time.sleep(1)
